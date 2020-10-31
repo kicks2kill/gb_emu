2577a267e8cb1e9cf6ddce02f97d4c2bb1049893
Orinxlm <w.spicerx@gmail.com>
2020-10-31 13:41:58 -0400

    adding a file called 'RomOnlyMemory' as a way to separate the memory structures for ease of debugging purposes later down the line.

--- Emu/RomOnlyMemory.cpp
+++ Emu/RomOnlyMemory.cpp
@@ -26,3 +26,73 @@ void RomOnlyMemory::Reset(bool bCGB)
     m_bCGB = bCGB;
 }
 
+uint8_t RomOnlyMemory::PerformRead(uint16_t address)
+{
+    if (address >= 0xA000 && address < 0xC000)
+    {
+        if(m_pCartridge->GetRAMSize() > 0)
+            return m_pMemory->Retrieve(address);
+        else
+        {
+            Log("**** -- Please add ram to cart. Attempting to read from RAM without ram in cart %X", address);
+            return 0xFF;
+        }
+    }
+    else
+        return m_pMemory->Retrieve(address);
+}
+
+void RomOnlyMemory::PerformWrite(uint16_t address, uint8_t value)
+{
+    if (address < 0x8000)
+    {
+        //ROM address 
+        Log("**** -- Attempting to write on ROM address %X %X", address, value);
+    }
+    else if(address > 0xA000 && address < 0xC000)
+    {
+        if(m_pCartridge->GetRAMSize() > 0)
+        {
+            m_pMemory->Load(address,value);
+        }
+        else
+        {
+            Log("**** -- Please add ram to cart. Attempting to read from RAM without ram in cart %X %X", address, value);
+        }
+    }
+    else
+        m_pMemory->Load(address,value);
+}
+
+
+void RomOnlyMemory::SaveRAM(std::ostream &file)
+{
+    Log("RomOnlyMemory saving RAM....");
+
+    for(int i = 0xA000; i < 0xC000; i++)
+    {
+        uint8_t ram_bytes = m_pMemory->Retrieve(i);
+        file.write(reinterpret_cast<const char*> (&ram_bytes), 1);
+    }
+    Log("RomOnlyMemory RAM save complete");
+}
+
+bool RomOnlyMemory::LoadRAM(std::istream &file, int32_t fileSize)
+{
+    Log("RomOnlyMemory loading RAM....");
+
+    if((fileSize > 0) && (fileSize != 0x2000))
+    {
+        Log("RomOnlyMemory is incorrect size. Expected: %d but found %d", 0x2000, fileSize);
+        return false;
+    }
+    for(int i = 0xA000; i < 0xC000; i++)
+    {
+        uint8_t ram_bytes = 0;
+        file.read(reinterpret_cast<char*>(&ram_bytes), 1);
+        m_pMemory->Load(i, ram_bytes);
+    }
+    Log("RomOnlyMemory Loading RAM complete");
+    
+    return true;
+}
